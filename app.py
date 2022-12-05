63b1eabc21b473c117046e51e2c3b1bee0c23ad6
pashkatrick <coolbatmanarrow@gmail.com>
2022-12-05 15:22:05 +0300

    [builder] add argparser

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, render_template
 from core import task_manager, message_manager, prepare_manager
 from decouple import config
+import argparse
 
 app = Flask(__name__, static_url_path='/static')
 mm = message_manager.MessageManager()
@@ -16,11 +17,12 @@ def hello_world():
 
 @app.route('/start')
 def start():
-    conf = request.args.get('config')
-    theme = request.args.get('theme')
-    # TODO: more exceptions and path validation
-    if conf or theme:
-        prep.init_config(conf, theme)
+    # TODO: it doesn't work
+    # conf = request.args.get('config')
+    # theme = request.args.get('theme')
+    # TODO: more exceptions and path validation via cmd
+    # if conf or theme:
+    #     prep.init_config(conf, theme)
     tm.init_cls(prep.prepare_cls())
     tm.start()
     return dict(data='started')
@@ -48,5 +50,10 @@ def listen():
 
 
 if __name__ == '__main__':
-    app.run(host=config('PU_ADDRESS'), port=config(
-        'PU_PORT'), debug=config('PU_DEBUG'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', action='store', default=config('PU_PORT'))    
+    parser.add_argument('-a', '--address', action='store', default=config('PU_ADDRESS'))   
+    parser.add_argument('-c', '--config', action='store', default=config('PU_CONFIG'))   
+    parser.add_argument('-t', '--theme', action='store', default=config('PU_THEME'))   
+    args = parser.parse_args()
+    app.run(host=args.address, port=args.port, debug=config('PU_DEBUG'))
